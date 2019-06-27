2dc2de32a861add267e211a2dcd4ef386eb787ba
devrajdunna <devrajdunna17@exgmail.com>
2019-06-27 19:53:23 +0530

    Devise Scaffold Linkup

--- app/controllers/myclicks_controller.rb
+++ app/controllers/myclicks_controller.rb
@@ -1,33 +1,32 @@
 class MyclicksController < ApplicationController
-  def index
-   @myclick = Myclick.all
-  end
+ def index
+  @myclick = Myclick.all
+ end
 
-  def new
-  	@myclick = Myclick.new
-  end
+ def new
+ 	@myclick = Myclick.new
+ end
 
-def save
-end
+ def save
+ end
 
-  def create
+ def create
   	@myclick = Myclick.new(myclick_params)
-    
-	if @myclick.save
-         redirect_to myclicks_path, notice: "The File #{@myclick.name} has been uploaded."
-      else
-         render "new"
-      end
-  end
+ if @myclick.save
+    redirect_to myclicks_path, notice: "The File #{@myclick.name} has been uploaded."
+ else
+   render "new"
+ end
+ end
 
-  def destroy
-  	@myclick = Myclick.find(params[:id])
-  	@myclick.destroy
-  	redirect_to myclicks_path, notice:"My File #{@myclick.name} has been deleted."
-  end
+ def destroy
+ 	@myclick = Myclick.find(params[:id])
+ 	@myclick.destroy
+ 	redirect_to myclicks_path, notice:"My File #{@myclick.name} has been deleted."
+ end
 
-  private
-      def myclick_params
-      params.require(:myclick).permit(:name, :attachment)
-   end
+private
+  def myclick_params
+    params.require(:myclick).permit(:name, :attachment)
+  end
 end
